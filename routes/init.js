a051c8b3a7b141d05e134b3c60a6078311360ce3
nikusha555 <nikushamitichashvili@gmail.com>
2024-12-18 09:32:03 -0800

    debugging

--- routes/init.js
+++ routes/init.js
@@ -1,7 +1,7 @@
-const appRoutes = require('./app.routes');
-const newsRoutes = require('./news/news.routes');
-const newsDetailsRoutes = require('./news/newsDetails.routes');
-const newsCategories = require('./news/newsCategories.routes');
+console.log('App Routes:', require.resolve('./App.routes'));
+console.log('News Routes:', require.resolve('./news/news.routes'));
+console.log('News Details:', require.resolve('./news/newsDetails.routes'));
+console.log('News Categories:', require.resolve('./news/newsCategories.routes'));
 
 module.exports = (app) => {
     app.use('/', appRoutes);
