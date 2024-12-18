03f33da74c1c786718c11fae3752f5dab7c82294
nikusha555 <nikushamitichashvili@gmail.com>
2024-12-18 09:35:25 -0800

    debugging

--- routes/init.js
+++ routes/init.js
@@ -1,7 +1,15 @@
-console.log('App Routes:', require.resolve('./App.routes'));
-console.log('News Routes:', require.resolve('./news/news.routes'));
-console.log('News Details:', require.resolve('./news/newsDetails.routes'));
-console.log('News Categories:', require.resolve('./news/newsCategories.routes'));
+const express = require('express');
+const appRoutes = require('./app.routes'); // Check case sensitivity
+const newsRoutes = require('./news/news.routes');
+const newsDetailsRoutes = require('./news/newsDetails.routes');
+const newsCategories = require('./news/newsCategories.routes');
+
+module.exports = (app) => {
+    app.use('/', appRoutes); // Ensure appRoutes is properly imported
+    app.use('/news', newsRoutes);
+    app.use('/newsDetails', newsDetailsRoutes);
+    app.use('/newsCategories', newsCategories);
+};
 
 module.exports = (app) => {
     app.use('/', appRoutes);
