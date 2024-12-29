e12f969bdd33f2e2545e6557fd8c5106cb052f48
nikusha555 <nikushamitichashvili@gmail.com>
2024-12-29 12:17:37 -0800

    updated cors params

--- server.js
+++ server.js
@@ -8,22 +8,13 @@ import cors from 'cors';
 // Load the .env.production file (or you can use .env by default in development)
 dotenv.config({ path: '.env' });  // Explicitly load .env.production in production
 
-const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:4200'];
 
 app.use(cors({
-    origin: (origin, callback) => {
-        // Allow requests with no origin (e.g., mobile apps, Postman)
-        if (!origin) return callback(null, true);
-
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true); // Allow this origin
-        } else {
-            callback(new Error('Not allowed by CORS')); // Block other origins
-        }
-    },
+    origin: ['http://localhost:4200'], // Front-end origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-    credentials: true, // Allow credentials (cookies, tokens)
+    credentials: true, // Include credentials if required
+
 }));
 
 
